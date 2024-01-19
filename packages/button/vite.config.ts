92c135b1c871c57be08c77d4ea0e5ab335bae36a
hdoc <hector.ochoa.dev@gmail.com>
2024-01-18 22:16:18 -0500

    build(button): remove unnecessary option

--- packages/button/vite.config.ts
+++ packages/button/vite.config.ts
@@ -7,11 +7,7 @@ import react from "@vitejs/plugin-react-swc";
 import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    libInjectCss(),
-    dts({ include: ["lib"], copyDtsFiles: true }),
-  ],
+  plugins: [react(), libInjectCss(), dts({ include: ["lib"] })],
   build: {
     lib: {
       entry: resolve(__dirname, "lib/main.ts"),
