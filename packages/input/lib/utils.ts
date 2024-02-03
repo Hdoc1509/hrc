5fa3eed7dd3a2f50e354db1964ad9cb79a3749b0
hdoc <hector.ochoa.dev@gmail.com>
2024-02-02 21:49:30 -0500

    feat(input): use border width for auto-resize textarea

--- packages/input/lib/utils.ts
+++ packages/input/lib/utils.ts
@@ -2,5 +2,7 @@ export const autosizeTextarea = (
   textarea: EventTarget & HTMLTextAreaElement,
 ) => {
   textarea.style.height = "auto";
-  textarea.style.height = textarea.scrollHeight + "px";
+  textarea.style.height = `${textarea.scrollHeight + 2}px`;
+  // NOTE: ↓ This for automatically taking into consideration the border
+  // textarea.style.height = `${textarea.scrollHeight + Number(getComputedStyle(textarea).borderWidth.slice(0, -2))* 2}px`
 };
