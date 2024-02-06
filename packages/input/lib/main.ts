d1b7b0f51952af592cd363d83a7fe3d8c4ec4b00
hdoc <hector.ochoa.dev@gmail.com>
2024-02-06 17:53:07 -0500

    refactor(input): remove unnecessary sass files

--- packages/input/lib/main.ts
+++ packages/input/lib/main.ts
@@ -1,4 +1,5 @@
-import "../lib/css/main.scss";
+import "./css/label.scss";
+import "./css/helper-text.scss";
 export { Input } from "./Input";
 export { Textarea } from "./Textarea";
 export { Checkbox } from "./Checkbox";
