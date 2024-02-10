761ec88f86d28a00638e1efbfa48e65dfc7a8591
hdoc <hector.ochoa.dev@gmail.com>
2024-02-10 16:44:43 -0500

    build(input): split up generated css

--- packages/input/vite.config.ts
+++ packages/input/vite.config.ts
@@ -1,16 +1,17 @@
 // https://vitejs.dev/config/
 import { defineConfig } from "vite";
-import { resolve } from "node:path";
+import { resolve, extname, relative } from "node:path";
+import { fileURLToPath } from "node:url";
+import { glob } from "glob";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 import dts from "vite-plugin-dts";
 import react from "@vitejs/plugin-react-swc";
-
-import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 export default defineConfig({
   plugins: [
     react(),
     libInjectCss(),
-    dts({ include: ["lib"], exclude: ["lib/utils.ts"] }),
+    dts({ include: ["lib"], exclude: ["lib/{utils.ts,Icons.tsx}"] }),
   ],
   build: {
     lib: {
@@ -20,6 +21,18 @@ export default defineConfig({
     copyPublicDir: false,
     rollupOptions: {
       external: ["react", "react/jsx-runtime", "clsx"],
+      input: Object.fromEntries(
+        glob
+          .sync("lib/*.{ts,tsx}")
+          .map((file) => [
+            relative("lib", file.slice(0, file.length - extname(file).length)),
+            fileURLToPath(new URL(file, import.meta.url)),
+          ]),
+      ),
+      output: {
+        assetFileNames: "[name][extname]",
+        entryFileNames: "[name].js",
+      },
     },
   },
 });
