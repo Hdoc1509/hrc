61d0b972ce41ff25f112af46299258396825971d
hdoc <hector.ochoa.dev@gmail.com>
2024-02-14 21:57:45 -0500

    build(button): simplify logic for set entry point

--- packages/button/vite.config.ts
+++ packages/button/vite.config.ts
@@ -1,6 +1,5 @@
 // https://vitejs.dev/config/
 import { defineConfig } from "vite";
-import { resolve } from "node:path";
 import dts from "vite-plugin-dts";
 import react from "@vitejs/plugin-react-swc";
 
@@ -10,7 +9,7 @@ export default defineConfig({
   plugins: [react(), libInjectCss(), dts({ include: ["lib"] })],
   build: {
     lib: {
-      entry: resolve(__dirname, "lib/main.ts"),
+      entry: "lib/main.ts",
       formats: ["es"],
     },
     copyPublicDir: false,
