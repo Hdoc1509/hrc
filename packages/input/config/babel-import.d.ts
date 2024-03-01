6b17b9aac2cf52f2bf426927d24710272939acb1
hdoc <hector.ochoa.dev@gmail.com>
2024-02-29 19:40:07 -0500

    feat(input): add config file for babel plugin

---
+++ packages/input/config/babel-import.d.ts
@@ -0,0 +1,1 @@
+declare module "@hdoc-react/input/config/babel-import"
---
+++ packages/input/config/babel-import.js
@@ -0,0 +1,9 @@
+export default [
+  "import",
+  {
+    libraryName: "@hdoc-react/input",
+    libraryDirectory: "dist",
+    camel2DashComponentName: false,
+    transformToDefaultImport: false,
+  },
+];
