7486185e4a9d735b1b6cf3b5239961d64be36351
hdoc <hector.ochoa.dev@gmail.com>
2024-03-16 17:33:58 -0500

    build(vite-config): update build folder

---
+++ packages/vite-config/build/scan.d.ts
@@ -0,0 +1,1 @@
+export declare const scanEntries: (entryDirs: string | string[]) => Record<string, string>;
---
+++ packages/vite-config/build/scan.js
@@ -0,0 +1,21 @@
+import path from "node:path";
+import fs from "node:fs";
+// Taken from https://github.com/emosheeep/vite-plugin-lib-inject-css/blob/81ba22b57e435ea813abfac717d426350773a16d/packages/vite-plugin-lib-inject-css/index.ts#L79
+export const scanEntries = (entryDirs) => {
+    const entries = {};
+    const counter = {};
+    for (const entryDir of [entryDirs].flat()) {
+        if (!entryDir)
+            break;
+        const flattenEntries = fs.statSync(entryDir).isDirectory()
+            ? fs.readdirSync(entryDir).map((name) => path.resolve(entryDir, name))
+            : [entryDir];
+        for (const entry of flattenEntries) {
+            const { name } = path.parse(entry);
+            const entryIndex = counter[name] || 0;
+            entries[`${name}${entryIndex || ""}`] = entry;
+            counter[name] = entryIndex + 1;
+        }
+    }
+    return entries;
+};
