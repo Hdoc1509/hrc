9bbabffcccefabcefbd62f0824f4b00a8ac7f61b
hdoc <hector.ochoa.dev@gmail.com>
2024-03-20 17:48:10 -0500

    feat(spinner): create internal component `<SpinnerBase />`

---
+++ packages/spinner/lib/components/SpinnerBase/index.tsx
@@ -0,0 +1,32 @@
+import clsx from "clsx";
+import "./style.scss";
+
+export type Spinner = {
+  color?: "primary" | "secondary" | "error" | "warning" | "info" | "success";
+  size?: "small" | "large";
+  speed?: "slow" | "fast";
+  className?: string;
+};
+
+type Props = Spinner & { name: string };
+
+export const SpinnerBase = ({
+  color,
+  size,
+  speed,
+  className,
+  children,
+  name,
+}: React.PropsWithChildren<Props>): JSX.Element => {
+  const spinnerClass = clsx(
+    ["spinner", name],
+    {
+      [`spinner--${color}`]: color,
+      [`spinner--${size}`]: size,
+      [`spinner--${speed}`]: speed,
+    },
+    className,
+  );
+
+  return <div className={spinnerClass}>{children}</div>;
+};
