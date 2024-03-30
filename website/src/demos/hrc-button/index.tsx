7591e6f1e8ab8b56abc3d0720421d68ffed0ce4c
hdoc <hector.ochoa.dev@gmail.com>
2024-03-29 22:53:13 -0500

    feat(website): include entry file of `@hrc/button` demos

---
+++ website/src/demos/hrc-button/index.tsx
@@ -0,0 +1,14 @@
+export { ButtonDefault } from "./button/default";
+export { ButtonColors } from "./button/colors";
+export { ButtonSizes } from "./button/sizes";
+export { ButtonRounded } from "./button/rounded";
+export { ButtonWithIcons } from "./button/icons";
+export { ButtonIconDefault } from "./button-icon/default";
+export { ButtonIconColors } from "./button-icon/colors";
+export { ButtonIconSizes } from "./button-icon/sizes";
+export { ButtonIconRounded } from "./button-icon/rounded";
+export { ButtonGroupDefault } from "./button-group/default";
+export { ButtonGroupColors } from "./button-group/colors";
+export { ButtonGroupSizes } from "./button-group/sizes";
+export { ButtonGroupColumn } from "./button-group/column";
+export { ButtonGroupWithIcons } from "./button-group/icons";
