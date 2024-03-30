196f93eb2f53fa10383e163fdff88b2c6ed8fe55
hdoc <hector.ochoa.dev@gmail.com>
2024-03-29 20:47:40 -0500

    feat(website): add script to copy demos

---
+++ website/scripts/copy-demos.sh
@@ -0,0 +1,21 @@
+demos_dir=./src/demos
+# demos_dir=./demos-test
+# PKGS=(button input material-icons spinner toggle-theme)
+PKGS=(button)
+
+for pkg in "${PKGS[@]}"; do
+  pkg_dir="../packages/${pkg}"
+  pkg_name=$(jq '.name' "${pkg_dir}"/package.json)
+  pkg_demo_target="${demos_dir}/hrc-${pkg}"
+
+  echo "Copying demos for ${pkg_name}..."
+
+  cp --recursive --update "${pkg_dir}"/src/demos "${pkg_demo_target}"
+
+  find "${pkg_demo_target}" -type f -print0 |
+    xargs -0 sed -i "1 s/@lib\/main/@hrc\/${pkg}/"
+
+  echo "Copied demos for ${pkg_name}"
+done
+
+find "${demos_dir}" -type f -name "index.tsx" -delete
