508346632da6a12df9c72c1a6665a7f17142c345
hdoc <hector.ochoa.dev@gmail.com>
2024-03-30 21:00:52 -0500

    refactor(website): remove typescript support for astro files

--- website/.eslintrc.cjs
+++ website/.eslintrc.cjs
@@ -4,10 +4,6 @@ module.exports = {
     {
       files: ["*.astro"],
       parser: "astro-eslint-parser",
-      parserOptions: {
-        parser: "@typescript-eslint/parser",
-        extraFileExtensions: [".astro"],
-      },
     }
   ]
 }
