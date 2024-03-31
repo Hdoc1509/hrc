1aa12876a5ac3ab07baaf453b3b51eaf6984bd8c
hdoc <hector.ochoa.dev@gmail.com>
2024-03-31 12:47:28 -0500

    refactor(website): use `React` type namespace in `<Demo />`

--- website/src/components/Demo.tsx
+++ website/src/components/Demo.tsx
@@ -1,8 +1,7 @@
 import { DemoDocs } from "@hrc/docs";
-import type { ComponentProps } from "react";
 import "./Demo.css";
 
-export const Demo = (props: ComponentProps<typeof DemoDocs>) => {
+export const Demo = (props: React.ComponentProps<typeof DemoDocs>) => {
   return (
     <DemoDocs className="demo">
       <DemoDocs {...props} />
