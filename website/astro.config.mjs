e94376a406f12f4ca40c0767e4fa953f7029210a
hdoc <hector.ochoa.dev@gmail.com>
2024-03-31 13:12:33 -0500

    ci: setup website deploy

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -5,6 +5,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://Hdoc1509.github.io",
+  base: "/hrc",
   integrations: [
     starlight({
       title: "<hrc />",
