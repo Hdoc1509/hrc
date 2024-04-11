15a7a8c07be1185ce4a61d27a77b439ace2206eb
hdoc <hector.ochoa.dev@gmail.com>
2024-04-11 11:29:16 -0500

    feat(website): add entry file for local components

---
+++ website/src/components/index.ts
@@ -0,0 +1,9 @@
+// FIX: Cannot find module './*.astro' or its corresponding type declarations.
+// see: https://github.com/withastro/language-tools/issues/584
+export { Demo } from "./Demo";
+// @ts-ignore
+export { default as SourceCode } from "./SourceCode.astro";
+// @ts-ignore
+export { default as SourceCodeWithDisabled } from "./SourceCodeWithDisabled.astro";
+// @ts-ignore
+export { default as MultiSourceCode } from "./MultiSourceCode.astro";
