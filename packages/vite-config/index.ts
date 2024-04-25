6f6ad84d89c710751c3e01901a4bdeb901a4d627
hdoc <hector.ochoa.dev@gmail.com>
2024-04-24 21:43:38 -0500

    refactor(vite-config)!: change way of scan files

--- packages/vite-config/index.ts
+++ packages/vite-config/index.ts
@@ -1,5 +1,6 @@
-import { scanEntries } from "./scan.js";
-import { resolve } from "path";
+import { glob } from "glob";
+import { extname, relative, resolve } from "path";
+import { fileURLToPath } from "url";
 
 type BuildOptions = {
   /** Set extra external dependencies
@@ -10,13 +11,6 @@ type BuildOptions = {
    * `main` is already set
    */
   extraEntries?: Record<string, string>;
-  /** Set entries based on `lib/components` directory structure
-   *
-   * If `true`, it will scan `lib/components` and add entries based on:
-   * 1: **If found a file**, use `filename` without extension as entry name
-   * 2: **If found a directory**, use `directoryname` as entry name
-   */
-  componentsDir?: boolean;
 };
 
 /**
@@ -29,15 +23,15 @@ const config = {
    * - `lib.formats`
    * - `copyPublicDir`
    * - `rollupOptions.external`
+   * - `rollupOptions.input`
    * - `rollupOptions.output.assetFileNames`
    * - `rollupOptions.output.entryFileNames`
    * */
-  build: ({ extraDeps, extraEntries, componentsDir }: BuildOptions = {}) => ({
+  build: (dirname: string, { extraDeps, extraEntries }: BuildOptions = {}) => ({
     lib: {
       entry: {
         main: "lib/main.ts",
         ...extraEntries,
-        ...(componentsDir ? scanEntries("lib/components") : {}),
       } as { main: string; [key: string]: string },
       formats: ["es"] as ["es"],
     },
@@ -48,6 +42,16 @@ const config = {
         "react/jsx-runtime",
         ...(extraDeps ? [extraDeps].flat() : []),
       ],
+      // Taken and adapted from:
+      // https://dev.to/receter/how-to-create-a-react-component-library-using-vites-library-mode-4lma#split-up-the-css
+      input: Object.fromEntries(
+        glob.sync(`${resolve(dirname)}/lib/**/*.{ts,tsx}`).map((file) => [
+          // entry point name
+          relative("lib", file.slice(0, file.length - extname(file).length)),
+          // entry point path
+          fileURLToPath(new URL(file, import.meta.url)),
+        ]),
+      ),
       output: {
         assetFileNames: "css/[name][extname]",
         entryFileNames: "[name].js",
