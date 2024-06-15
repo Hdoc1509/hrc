7cc257c7a1c402d92a322ec0baa73adfad4fbf8c
hdoc <hector.ochoa.dev@gmail.com>
2024-06-15 18:09:17 -0500

    fix(website): allow re-export of `.astro` files from `.ts` files

--- website/src/components/index.ts
+++ website/src/components/index.ts
@@ -1,11 +1,5 @@
-// FIX: Cannot find module './*.astro' or its corresponding type declarations.
-// see: https://github.com/withastro/language-tools/issues/584
 export { Demo } from "./Demo";
-// @ts-ignore
 export { default as SourceCode } from "./SourceCode.astro";
-// @ts-ignore
 export { default as SourceCodeWithDisabled } from "./SourceCodeWithDisabled.astro";
-// @ts-ignore
 export { default as MultiSourceCode } from "./MultiSourceCode.astro";
-// @ts-ignore
 export { default as InstallCommand } from "./InstallCommand.astro";
