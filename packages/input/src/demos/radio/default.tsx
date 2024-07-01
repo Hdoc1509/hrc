cd68bd13f7701054da2822b519b2d1f4e34ae9e2
hdoc <hector.ochoa.dev@gmail.com>
2024-06-30 22:16:15 -0500

    refactor(input): change `name` used in demo for default `<Radio />`

--- packages/input/src/demos/radio/default.tsx
+++ packages/input/src/demos/radio/default.tsx
@@ -3,8 +3,8 @@ import { Radio } from "@lib/main";
 export function RadioDefault() {
   return (
     <>
-      <Radio label="Write a story" name="story-action" />
-      <Radio label="Read a story" name="story-action" defaultChecked />
+      <Radio label="Write a story" name="radio-default" />
+      <Radio label="Read a story" name="radio-default" defaultChecked />
     </>
   );
 }
