e4b67a6aa6b930c93d1c4e45c0c70ffe4afc6097
hdoc <hector.ochoa.dev@gmail.com>
2024-07-03 22:04:54 -0500

    feat(input): set `value` prop of `<Radio />` as required

--- packages/input/lib/Radio/types.ts
+++ packages/input/lib/Radio/types.ts
@@ -2,8 +2,8 @@ import { Simplify } from "@hrc/type-utils";
 import { CheckboxProps } from "../Checkbox/types";
 
 export type RadioProps = Simplify<
-  Omit<CheckboxProps, "label" | "name" | "icon" | "iconChecked"> & {
+  Omit<CheckboxProps, "label" | "icon" | "iconChecked" | "value"> & {
+    value: string;
     label: string;
-    name: string;
   }
 >;
