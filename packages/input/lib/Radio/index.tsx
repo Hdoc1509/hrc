c12cf5aacb4e95eed4bbbfbefd0ab4f960f3be4d
hdoc <hector.ochoa.dev@gmail.com>
2024-07-03 22:23:49 -0500

    feat(input): consume `RadioGroupContext` in `<Radio />`

--- packages/input/lib/Radio/index.tsx
+++ packages/input/lib/Radio/index.tsx
@@ -1,3 +1,4 @@
+import { useRadioGroupContext } from "../RadioGroup/context";
 import clsx from "clsx";
 import { Label } from "../Label";
 import { RadioChecked, RadioUnchecked } from "../Icons";
@@ -12,14 +13,27 @@ export const Radio = ({
   className,
   disabled,
   required,
+  name,
+  form,
+  defaultChecked,
+  value,
+  onChange,
   ...restProps
 }: RadioProps) => {
+  const group = useRadioGroupContext();
+
+  let isDefaultChecked = defaultChecked;
+
+  if ((group?.defaultValue != null || group?.value != null) && value != null) {
+    isDefaultChecked = group.defaultValue === value || group.value === value;
+  }
+
   const wrapperClass = clsx(
     "radio",
     {
-      [`radio--${size}`]: size,
+      [`radio--${size ?? group?.size}`]: size ?? group?.size,
       "radio--disabled": disabled,
-      [`radio--${color}`]: color,
+      [`radio--${color ?? group?.color}`]: color ?? group?.color,
     },
     className,
   );
@@ -33,6 +47,11 @@ export const Radio = ({
           type="radio"
           disabled={disabled}
           required={required}
+          name={name ?? group?.name}
+          form={form ?? group?.form}
+          value={value}
+          onChange={onChange ?? group?.onChange}
+          defaultChecked={isDefaultChecked}
         />
         <span className="radio__icon">
           <RadioUnchecked />
