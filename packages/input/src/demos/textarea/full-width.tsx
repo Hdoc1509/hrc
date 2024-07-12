0da030a4d8269e14ddca573dd3ee3194bc35fa25
hdoc <hector.ochoa.dev@gmail.com>
2024-07-11 21:00:53 -0500

    fix(input): set correct label to demo of `<Textarea />` with full width

--- packages/input/src/demos/textarea/full-width.tsx
+++ packages/input/src/demos/textarea/full-width.tsx
@@ -1,5 +1,5 @@
 import { Textarea } from "@lib/main";
 
 export function TextareaFullWidth() {
-  return <Textarea placeholder="Small" fullWidth />;
+  return <Textarea placeholder="Full Width" fullWidth />;
 }
