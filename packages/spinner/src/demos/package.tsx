9ed0dd306597b0b9a2b93e7e827758b04b8dbde0
hdoc <hector.ochoa.dev@gmail.com>
2024-07-16 17:14:13 -0500

    refactor(spinner): include rest of spinner in package demo

--- packages/spinner/src/demos/package.tsx
+++ packages/spinner/src/demos/package.tsx
@@ -1,11 +1,19 @@
-import { DualRingSpinner, GrowingSpinner, RingSpinner } from "@lib/main";
+import {
+  BorderSpinner,
+  CircleSpinner,
+  DualRingSpinner,
+  GrowingSpinner,
+  RingSpinner,
+} from "@lib/main";
 
 export function PackageDemo() {
   return (
     <>
-      <RingSpinner size="large" color="primary" />
-      <DualRingSpinner color="error" />
-      <GrowingSpinner />
+      <BorderSpinner size="large" color="primary" />
+      <CircleSpinner color="error" speed="slow" />
+      <DualRingSpinner size="small" />
+      <GrowingSpinner color="warning" speed="fast" />
+      <RingSpinner size="large" color="success" />
     </>
   );
 }
