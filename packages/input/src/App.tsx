99cf8bc05bd12c4bb1cf1035f696700980b5426a
hdoc <hector.ochoa.dev@gmail.com>
2024-08-17 17:23:44 -0500

    refactor(input): use dev-app styles from `@hrc/sass-utils`

--- packages/input/src/App.tsx
+++ packages/input/src/App.tsx
@@ -4,18 +4,11 @@ import { TextareaDemo } from "./components/TextareaDemo";
 import { CheckboxDemo } from "./components/CheckboxDemo";
 import { RadioDemo } from "./components/RadioDemo";
 import { RadioGroupDemo } from "./components/RadioGroupDemo";
-import "./App.css";
 import "./demos.css";
-
-const toggleTheme = () => {
-  document.body.dataset.theme =
-    document.body.dataset.theme === "dark" ? "light" : "dark";
-};
 
 function App() {
   return (
     <>
-      <button onClick={toggleTheme}>toggle theme</button>
       <Demo />
       <InputDemo />
       <TextareaDemo />
