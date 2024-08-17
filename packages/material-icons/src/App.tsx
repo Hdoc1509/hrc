2c6680ecb9d6bc4a2b6e72367b34db9dfd96f834
hdoc <hector.ochoa.dev@gmail.com>
2024-08-17 17:32:08 -0500

    refactor(material-icons): remove unnecesary elements from dev-app

--- packages/material-icons/src/App.tsx
+++ packages/material-icons/src/App.tsx
@@ -1,34 +1,12 @@
-import { useState } from "react";
-import { Icon } from "../lib/main";
 import { Demo } from "./components/Demo";
 import { IconDemo } from "./components/IconDemo";
 import "./App.css";
 
 function App() {
-  const [isDisabled, setIsDisabled] = useState(false);
-
-  const toggleError = () => setIsDisabled(!isDisabled);
-
   return (
     <>
-      <button onClick={toggleError}>Toggle disabled</button>
-      <div className="icon-container">
-        <Icon name="error" variant="outlined" size="large" />
-      </div>
-      <div className="my-container-with-custom-color">
-        <Icon
-          name="shopping_cart"
-          variant="outlined"
-          size="large"
-          color="error"
-          disabled={isDisabled}
-        />
-      </div>
       <Demo />
       <IconDemo />
-      <div className="inherit-size">
-        <Icon name="people" color="info" size="inherit" />
-      </div>
     </>
   );
 }
