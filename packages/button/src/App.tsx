081bd53a3c89b7cb370fa8383ad0edf070d76b75
hdoc <hector.ochoa.dev@gmail.com>
2024-08-17 16:45:45 -0500

    refactor(button): remove App.css from dev-app

--- packages/button/src/App.tsx
+++ packages/button/src/App.tsx
@@ -2,7 +2,6 @@ import { Demo } from "./components/Demo";
 import { ButtonDemo } from "./components/ButtonDemo";
 import { ButtonIconDemo } from "./components/ButtonIconDemo";
 import { ButtonGroupDemo } from "./components/ButtonGroupDemo";
-import "./App.css";
 import "./demos.css";
 
 function App() {
