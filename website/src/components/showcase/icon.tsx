bca781c9f44c0c979fc44954bb4b92de29173cb7
Hdoc1509 <hector.ochoa.dev@gmail.com>
2026-01-13 18:40:53 -0500

    feat(website): add showcase of font icons

---
+++ website/src/components/showcase/icon.tsx
@@ -0,0 +1,16 @@
+import { Icon } from "@hrc/material-icons";
+
+export function IconShowcase() {
+  return (
+    <div className="icon-showcase">
+      <Icon name="add_chart" color="success" />
+      <Icon name="api" variant="sharp" color="info" />
+      <Icon name="auto_awesome" color="warning" />
+      <Icon name="build" variant="sharp" color="primary" />
+      <Icon name="auto_awesome_motion" variant="round" color="primary" />
+      <Icon name="dns" variant="round" color="error" />
+      <Icon name="backup" variant="outlined" color="success" />
+      <Icon name="bookmark_add" variant="outlined" color="info" />
+    </div>
+  );
+}
