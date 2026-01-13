48c896bb108e3bc8f86513585305e052d45946cd
Hdoc1509 <hector.ochoa.dev@gmail.com>
2026-01-12 21:35:03 -0500

    feat(website): complete showcase for inputs

--- website/src/components/showcase/input.tsx
+++ website/src/components/showcase/input.tsx
@@ -1,10 +1,11 @@
-import { Input } from "@hrc/input";
+import { Checkbox, Input } from "@hrc/input";
 import { MagnifierIcon } from "../Icons";
 
 export function InputShowcase() {
   return (
     <div className="input-showcase">
       <Input iconStart={<MagnifierIcon />} placeholder="Search..." fullWidth />
+      <Checkbox label="Awesome" color="success" />
     </div>
   );
 }
